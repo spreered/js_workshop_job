cd5e5625190994c830692eef709c33697e6abfff
BeastRush1231 <M0761005@g2.usc.edu.tw>
2020-04-26 21:22:04 +0800

    navbar-burger active with jQuery

---
+++ index.js
@@ -0,0 +1,8 @@
+// task1_jQuery
+  $(document).ready(function() {
+    $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+    });
+  });
+// task1_jQuery
