497eaf29b740344062e3d6796e6b7e1182e5a241
eudora-hsj <eudora.hsj@gmail.com>
2020-04-28 16:30:37 +0800

    task_4

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -3,34 +3,52 @@
 $(document).ready(function () {
   // task1
   var menuBtn = document.querySelector("#navbar-burger");
+  var pageBtn = document.querySelector(".pagination .pagination-next");
   var menuBody = document.querySelector("#navbar-menu");
   menuBtn.addEventListener("click", menuRWD);
+  pageBtn.addEventListener("click", nextPage);
+  var pageNow = 1;
 
   function menuRWD() {
     this.classList.toggle("is-active");
     menuBody.classList.toggle("is-active");
-  } // task2.task3
+  }
 
+  function nextPage() {
+    pageNow = pageNow + 1;
+    searchJson(pageNow);
+  }
 
   var form = document.forms["search-job"];
   var search = form.search;
   search.addEventListener("click", searchJson);
 
-  function searchJson() {
+  function searchJson(pageNow) {
     event.preventDefault();
     var keyworddDescription = form.description.value;
     var keywordLocation = form.location.value;
     var keywordFulltime = form.full_time.checked ? "Full Time" : "";
-    console.log(keywordFulltime);
-    var url = "https://still-spire-37210.herokuapp.com/positions.json?description=".concat(keyworddDescription, "&location=").concat(keywordLocation, "&type=").concat(keywordFulltime, " ");
+    var page = pageNow;
+    var test = 0; // let url = `https://still-spire-37210.herokuapp.com/positions.json?description=${keyworddDescription}&location=${keywordLocation}&type=${keywordFulltime} `
+
+    var url = "https://still-spire-37210.herokuapp.com/positions.json?description=".concat(keyworddDescription, "&location=").concat(keywordLocation, "&type=").concat(keywordFulltime, "&page=").concat(page, " ");
     axios.get(url).then(function (response) {
+      var dataCount = response.data.length;
+
+      if (dataCount >= 50) {
+        pageBtn.removeAttribute('disabled');
+      } else {
+        pageBtn.setAttribute('disabled', true);
+      }
+
       response.data.map(function (x) {
         $('#job-pannel').append(toPostHtml(x));
       });
     });
 
     function toPostHtml(jsn) {
-      return "\n                <tr>\n                    <td>\n                        <h4><a href=\" ".concat(jsn.url, "\">").concat(jsn.title, "</a></h4>\n                        <p class=\"source\">\n                            <a class=\"company\" href=\"").concat(jsn.company_url, "\">").concat(jsn.company, "</a> \u2013\n                            <strong class=\"fulltime\">Full Time</strong>\n                        </p>\n                    </td>\n                    <td class=\"meta\">\n                        <span class=\"location\">").concat(jsn.location, "</span>\n                    </td>\n                </tr>\n                ");
+      test = test + 1;
+      return "\n                <tr>\n                    <td>\n                    <p>".concat(test, "</p>\n                        <h4><a href=\" ").concat(jsn.url, "\">").concat(jsn.title, "</a></h4>\n                        <p class=\"source\">\n                            <a class=\"company\" href=\"").concat(jsn.company_url, "\">").concat(jsn.company, "</a> \u2013\n                            <strong class=\"fulltime\">Full Time</strong>\n                        </p>\n                    </td>\n                    <td class=\"meta\">\n                        <span class=\"location\">").concat(jsn.location, "</span>\n                    </td>\n                </tr>\n                ");
     }
   }
 });
