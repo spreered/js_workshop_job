76854b19113d4f55e7a9aef6209ad255d8d6c027
lynnchang <tv725987@gmail.com>
2020-04-28 17:21:21 +0800

    add cross

--- feature1.js
+++ feature1.js
@@ -2,5 +2,9 @@ let burger = document.querySelector('.navbar-burger')
 let burgerMenu = document.getElementById('navbar-menu')
 
 burger.addEventListener('click',function(){
+  burger.classList.toggle('is-active');
+})
+
+burger.addEventListener('click',function(){
   burgerMenu.classList.toggle('is-active');
 })
