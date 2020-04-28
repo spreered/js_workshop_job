33316a0b9e5d2e0e6a5b7d441ec78699a72f7b3d
Joseph Hung <arixxle@gmail.com>
2020-04-28 12:30:16 +0800

    toggle nav bar

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('lalalalala');
+
+let ham = document.querySelector('#navbar-burger')
+var menu = document.querySelector('#navbar-menu')
+
+ham.addEventListener('click', function () {
+  menu.classList.toggle('is-active');
+})
