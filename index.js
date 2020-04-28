717587b5430aafcbd8de44e2c8489d2b6cb6cfa6
Aweiisacat <face992302@gmail.com>
2020-04-28 14:24:47 +0800

    Task2

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 let btn = document.querySelector('.navbar-burger')
 let menu = document.querySelector('.navbar-menu')
+let form = document.querySelector('form')
+let input = document.querySelectorAll('input')
+let html = {}
 btn.addEventListener('click',function(){
   btn.classList.toggle('is-active')
   menu.classList.toggle('is-active')
+  return false
 })
+form.addEventListener('submit',function(){
+  let description = document.getElementsByName('description')[0].value;
+  let country = document.getElementsByName('location')[0].value;
+  let full_time = document.getElementsByName('full_time')[0].checked;
+  html['workshop'] = `https://jobs.github.com/position.json?description=${description}&location=${country}&full_time=${full_time}`
+  console.log(html.workshop)
+  event.preventDefault()
+  return false
+})
+let html2 = fetch('https://still-spire-37210.herokuapp.com/positions.json')
+  .then( (resp) => {
+    console.log(resp.json())
+  })
+
+
+
