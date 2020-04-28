34a67fb03f639d57d832cc0c46b96024a18de5e8
Gao Ciaorun <itchynozz@gmail.com>
2020-04-28 15:47:22 +0800

    hamburger

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let burger = document.querySelector(".navbar-burger.burger");
 burger.addEventListener("click", function(){
   console.log("click!")
-  // if (this.classList.contains('is-active'))
-  //   this.classList.remove("is-active")
-  // else
-  //   this.classList.add('is-active')
-  let classes = this.className.split(' ')
-  if (classes.includes('is-active'))
-    classes = classes.filter((cls) => cls !== 'is-active')
+  if (this.classList.contains('is-active'))
+    this.classList.remove("is-active")
   else
-    classes.push('is-active')
-  this.className = classes.join(' ')
+    this.classList.add('is-active')
+  
 })
 
 
-
+// let classes = this.className.split(' ')
+//   if (classes.includes('is-active'))
+//     classes = classes.filter((cls) => cls !== 'is-active')
+//   else
+//     classes.push('is-active')
+//   this.className = classes.join(' ')
