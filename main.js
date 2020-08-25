fc75b93aba90336ed2bd67502104ad2a2c47409b
Leoyang0717 <pithend58639@gmail.com>
2020-08-25 14:41:06 +0800

    finsh

--- main.js
+++ main.js
@@ -5,4 +5,27 @@ burger.addEventListener('click',function(){
   burger.classList.toggle('is-active')
   menu.classList.toggle('is-active')
   })
+
+const form = document.forms['search-job']
+
+form.addEventListener('submit',function(e){
+  e.preventDefault()
+  // form.elements[0].value
+  // form.elements[1].value
+  // form.elements[2].checked
+  fetchJob()
+  })
+
+  
+  function fetchJob(){
+    if (form.elements[2].checked){
+      fetch(`https://still-spire-37210.herokuapp.com/positions.json?utf8=%E2%9C%93&description=${form.elements[0].value}&location=${form.elements[1].value}&full_time=on`)
+      .then((resquest) => resquest.json())
+      .then(posts => console.log(posts))
+    } else {
+      fetch(`https://still-spire-37210.herokuapp.com/positions.json?utf8=%E2%9C%93&description=${form.elements[0].value}&location=${form.elements[1].value}`)
+      .then((resquest) => resquest.json())
+      .then(posts => console.log(posts))
+    }
+  }
 })
