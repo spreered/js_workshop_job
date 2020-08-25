dfa0cc2ab03331421cab0c4524a3a0473274efa9
hank <hank8451@gmail.com>
2020-08-25 14:28:29 +0800

    成功印出搜尋結果

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   document.querySelector('#navbar-burger').addEventListener('click', ()=>{
+    
     const menu = document.querySelector('#navbar-menu');
     if (menu.classList.contains('is-active')){
       menu.classList.remove('is-active')
@@ -7,4 +8,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
       menu.classList.add('is-active')
     }
   })
+  formsFindJob = document.forms["search-job"]
+  formsFindJob.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    jobDes = formsFindJob[name="description"].value
+    jobLoc = formsFindJob[name="location"].value
+    jobFullTime = formsFindJob[name="full_time"].checked
+    // console.log(jobFullTime)
+    if (!!jobDes || !!jobLoc || jobFullTime){
+      fetch(`https://still-spire-37210.herokuapp.com/positions.json?description=${jobDes}&full_time=${jobFullTime}&location=${jobLoc}`)
+      .then(response => response.json())
+      .then(searchData => {
+        console.log(searchData)
+      })
+    }   
+ })
 })
