ba154bae08f93898a5b1df398c9dee4480d6afc0
Leoyang0717 <pithend58639@gmail.com>
2020-08-25 13:44:10 +0800

    finsh

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded',function(){
+const burger = document.querySelector('#navbar-burger')
+const menu = document.querySelector('#navbar-menu')
+burger.addEventListener('click',function(){
+  burger.classList.toggle('is-active')
+  menu.classList.toggle('is-active')
+  })
+})
